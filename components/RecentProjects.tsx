395537fc93fe56f4a474744e657cd6a275689390
Nahuel7709 <nahuelmenichetti98@gmail.com>
2024-06-22 20:55:54 -0300

    projects section update

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -26,7 +26,7 @@ const RecentProjects = () => {
         {projects.map(({ id, title, des, img, iconLists, link }) => (
           <div key={id} className='sm:h-[41rem] h-[31rem] lg:min-h-[32.5rem] flex items-center justify-center sm:w-[570px] w-[80vw]'>
             <PinContainer title={link} href={link}>
-              <div className='relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden sm:h-[40vh] h-[30vh] mb-10'>
+              <div className='relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden sm:h-[40vh] h-[30vh] mb-10 rounded-3xl'>
                 <div className='relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]'>
                   <img src="./bg.png" alt="bg-img" />
                 </div>
