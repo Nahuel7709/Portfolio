92c1941758d01aee63955480bff8e7c120063709
Nahuel7709 <nahuelmenichetti98@gmail.com>
2024-06-23 10:39:56 -0300

    fix nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output:'export',
+    output: 'export',
     typescript: {
         ignoreBuildErrors: true,
-    }
+    },
+    eslint: {
+        // This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
 };
 
 export default nextConfig;
