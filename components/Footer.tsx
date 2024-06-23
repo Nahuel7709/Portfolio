e9c7a8bf77cb3aa1741e49dea136cb01030bdf8d
Nahuel7709 <nahuelmenichetti98@gmail.com>
2024-06-23 10:34:22 -0300

    out folder

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
     <footer className='w-full pb-10 mb-[100px] md:mb-5' id='contact'>
         <div className='flex flex-col items-center'> 
             <h1 className='heading lg:max-w-[45vw]'>
-               Let's connect and discuss how <span className='text-purple'>we can work together.</span>
+               Let&apos;s connect and discuss how <span className='text-purple'>we can work together.</span>
             </h1>
             <p className='text-white-200 md:mt-10 my-5 text-center'>Reach out to me today, and let's explore how I can assist you in achieving your goals.</p>
             <a href="mailto:nahuel.d.menichetti@gmail.com">
