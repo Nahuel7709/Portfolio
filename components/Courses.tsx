6129eb43ce3be38dc0c0cbd2e4e11fb92d8c82b5
Nahuel7709 <nahuelmenichetti98@gmail.com>
2024-07-07 18:50:59 -0300

    new projects and courses edit

--- components/Courses.tsx
+++ components/Courses.tsx
@@ -5,28 +5,27 @@ import { InfiniteMovingCards } from './ui/InfiniteMovingCards'
 
 const Courses = () => {
   return (
-    <div className='py-20' id='courses'>
-      <h1 className='heading'>
-      Completed and Ongoing 
-        <span className='text-purple'> Education </span>
+    <div className="py-20" id="courses">
+      <h1 className="heading">
+        Completed and Ongoing
+        <span className="text-purple"> Education </span>
       </h1>
-      <div className='flex flex-col items-center max-lg:mt-10'>
-          <InfiniteMovingCards 
-            items={testimonials}
-            direction="right"
-            speed="slow"
-          /> 
-      <div className='flex flex-wrap items-center justify-center gap-4 md:gap-16 max-lg mt-10'>
-        {companies.map(({id, img, name, nameImg})=>(
-          <div key={id} className='flex md:max-w-60 max-w-32 gap-2'>
-            <img src={img} alt={name} className=' w-40 max-w-60 '/>
-            
-          </div>
-        ))}
-      </div>
+      <div className="flex flex-col items-center max-lg:mt-10">
+        <InfiniteMovingCards
+          items={testimonials}
+          direction="right"
+          speed="slow"
+        />
+        <div className="flex flex-col items-center justify-center gap-4 md:flex-row md:gap-16 max-lg mt-10">
+          {companies.map(({ id, img, name, nameImg }) => (
+            <div key={id} className="flex md:max-w-60 max-w-32 gap-2 ">
+              <img src={img} alt={name} className=" w-40 max-w-60 " />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Courses
